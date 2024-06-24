fb928095c1914bae4772772ffb6bff2bacdbde0f
sandali-aviha <dilsandu96@gmail.com>
2024-06-24 09:40:43 +0530

    Updates

---
+++ precache-manifest.c4176f9c1001dbb9364a7b95f1e3f65c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec62cc6a13aa8904fd4953639361f641",
+    "url": "/officialweb/index.html"
+  },
+  {
+    "revision": "5feab3f299b0ebfe0d94",
+    "url": "/officialweb/static/css/2.bfe40404.chunk.css"
+  },
+  {
+    "revision": "c78447f2cd327af11630",
+    "url": "/officialweb/static/css/main.f7b6b829.chunk.css"
+  },
+  {
+    "revision": "5feab3f299b0ebfe0d94",
+    "url": "/officialweb/static/js/2.b81bed17.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/officialweb/static/js/2.b81bed17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78447f2cd327af11630",
+    "url": "/officialweb/static/js/main.1c8a61c2.chunk.js"
+  },
+  {
+    "revision": "0fcc00e53ca98552dd5b",
+    "url": "/officialweb/static/js/runtime-main.84b813e5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/officialweb/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/officialweb/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/officialweb/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/officialweb/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/officialweb/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/officialweb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/officialweb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/officialweb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/officialweb/static/media/slick.f97e3bbf.svg"
+  }
+]);
